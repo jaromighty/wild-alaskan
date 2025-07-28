054af64b24f1d326c4c15c0d9f5f944afe3b69da
Jarom Rial <jarom@firebirdtech.dev>
2025-07-28 07:23:26 -0600

    update routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\RecipeSearchController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,4 +13,3 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('search', RecipeSearchController::class)->name('search');
